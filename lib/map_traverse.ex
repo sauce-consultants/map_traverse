66d60c0ae1da2b530453f2a409cde41551753688
Matt Weldon <matt@mattweldon.com>
2017-10-05 22:46:53 +0100

    Chore: Initial commit

---
+++ lib/map_traverse.ex
@@ -0,0 +1,18 @@
+defmodule MapTraverse do
+  @moduledoc """
+  Documentation for MapTraverse.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MapTraverse.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/map_traverse_test.exs
@@ -0,0 +1,8 @@
+defmodule MapTraverseTest do
+  use ExUnit.Case
+  doctest MapTraverse
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
